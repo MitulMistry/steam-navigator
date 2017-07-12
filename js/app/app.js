db6cd3c27879c0e9a361b6ffefa7837c1b461670
MitulMistry <MitulMistryDev@gmail.com>
2017-07-12 17:27:15 -0500

    Add routes

--- js/app/app.js
+++ js/app/app.js
@@ -2,4 +2,39 @@ angular
   .module('app', [ //define module and include dependencies
     'ui.router', //for routing
     'ngMaterial' //Angular material
-  ]);
+  ])
+  .config(function($stateProvider, $urlRouterProvider){ //inject $stateProvider for ui-router and $urlRouterProvider for 'otherwise' method
+    $stateProvider
+      .state('home', { //create a ui-router state
+        url: '/',
+        templateUrl: 'views/home.html',
+        controller: 'HomeController as ctrl' //set controller for this route (equavelent of using <ng-controller="..."> in template)
+      })
+      .state('home.topSellers', {
+        url: 'top',
+        templateUrl: 'views/games/index.html',
+        controller: 'GamesController as ctrl'
+      })
+      .state('home.newReleases', {
+        url: 'new',
+        templateUrl: 'views/games/index.html',
+        controller: 'GamesController as ctrl'
+      })
+      .state('home.specials', {
+        url: 'specials',
+        templateUrl: 'views/games/index.html',
+        controller: 'GamesController as ctrl'
+      })
+      .state('home.comingSoon', {
+        url: 'coming',
+        templateUrl: 'views/games/index.html',
+        controller: 'GamesController as ctrl'
+      })
+      .state('home.game', {
+        url: 'games/:id',
+        templateUrl: 'views/games/show.html',
+        controller: 'GameController as ctrl'
+      });
+
+      $urlRouterProvider.otherwise('/'); //default route
+  });
