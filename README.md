2b6ac44404c448f4e3472ff112cef385dac56353
MitulMistry <MitulMistryDev@gmail.com>
2017-07-15 00:14:31 -0500

    Refactor API calls in GamesService

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ A front end AngularJS web application for viewing the Steam Store
 
 To run the local dev server at http://localhost:8080, run `npm install`, then `node server.js`.
 
+For Chrome in a dev environment, I use [this extension][https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi] to deal with CORS for AJAX requests.
+
 Uses the [Steam Store web API][https://wiki.teamfortress.com/wiki/User:RJackson/StorefrontAPI].
--- js/app/services/GamesService.js
+++ js/app/services/GamesService.js
@@ -1,51 +1,30 @@
 var API_URL = 'http://store.steampowered.com/api';
-var featuredResponse;
+var featuredCategories;
 
 function GamesService($http, $sce) {
-  // this.trustSrc = function(src) {
-  //   return $sce.trustAsResourceUrl(src);
-  // };
-
-  this.getFeatured = function() {
-    if (!featuredResponse) { //If the response hasn't already been stored
-      // var trustedUrl = this.trustSrc(API_URL + '/featuredcategories');
-      // $http({
-      //   method: 'JSONP',
-      //   url: trustedUrl
-      // }).then(function successCallback(response) { // this callback will be called asynchronously when the response is available
-      //   featuredResponse = response;
-      //   return featuredResponse;
-      // }, function errorCallback(response) { // called asynchronously if an error occurs or server returns response with an error status.
-      //   console.log("API Error");
-      //   console.log(trustedUrl);
-      //   console.log(response);
-      // });
-
-      // $http.jsonp(trustedUrl, {jsonpCallbackParam: 'callback'}).then(function(response) {
-      //   console.log(response);
-      //   return response;
-      // });
-
+  this.getFeaturedCategories = function() {
+    if (!featuredCategories) { //If the response hasn't already been stored
+      featuredCategories = $http.get(API_URL + '/featuredcategories');
+      return featuredCategories;
     } else {
-      return featuredResponse;
+      return featuredCategories;
     }
   }
 
   this.getTopSellers = function() {
-    return $http.get(API_URL + '/featuredcategories')["top_sellers"];
-    // return this.getFeatured();
+    return this.getFeaturedCategories()["top_sellers"];
   }
 
   this.getNewReleases = function() {
-    return $http.get(API_URL + '/featuredcategories')["new_releases"];
+    return this.getFeaturedCategories()["new_releases"];
   }
 
   this.getSpecials = function() {
-    return $http.get(API_URL + '/featuredcategories')["specials"];
+    return this.getFeaturedCategories()["specials"];
   }
 
   this.getComingSoon = function() {
-    return $http.get(API_URL + '/featuredcategories')["coming_soon"];
+    return this.getFeaturedCategories()["coming_soon"];
   }
 
   this.getGame = function(id) {
