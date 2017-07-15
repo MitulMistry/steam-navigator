217938d469e941615c97af0d91679134985a8cc4
MitulMistry <MitulMistryDev@gmail.com>
2017-07-15 00:05:55 -0500

    Update GamesService API calls

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A front end AngularJS web application for viewing the Steam Store
 
 To run the local dev server at http://localhost:8080, run `npm install`, then `node server.js`.
+
+Uses the [Steam Store web API][https://wiki.teamfortress.com/wiki/User:RJackson/StorefrontAPI].
--- js/app/services/GamesService.js
+++ js/app/services/GamesService.js
@@ -1,20 +1,51 @@
 var API_URL = 'http://store.steampowered.com/api';
+var featuredResponse;
 
-function GamesService($http) {
+function GamesService($http, $sce) {
+  // this.trustSrc = function(src) {
+  //   return $sce.trustAsResourceUrl(src);
+  // };
+
+  this.getFeatured = function() {
+    if (!featuredResponse) { //If the response hasn't already been stored
+      // var trustedUrl = this.trustSrc(API_URL + '/featuredcategories');
+      // $http({
+      //   method: 'JSONP',
+      //   url: trustedUrl
+      // }).then(function successCallback(response) { // this callback will be called asynchronously when the response is available
+      //   featuredResponse = response;
+      //   return featuredResponse;
+      // }, function errorCallback(response) { // called asynchronously if an error occurs or server returns response with an error status.
+      //   console.log("API Error");
+      //   console.log(trustedUrl);
+      //   console.log(response);
+      // });
+
+      // $http.jsonp(trustedUrl, {jsonpCallbackParam: 'callback'}).then(function(response) {
+      //   console.log(response);
+      //   return response;
+      // });
+
+    } else {
+      return featuredResponse;
+    }
+  }
+
   this.getTopSellers = function() {
-    return $http.get(API_URL + '/featured')["top_sellers"];
+    return $http.get(API_URL + '/featuredcategories')["top_sellers"];
+    // return this.getFeatured();
   }
 
   this.getNewReleases = function() {
-    return $http.get(API_URL + '/featured')["new_releases"];
+    return $http.get(API_URL + '/featuredcategories')["new_releases"];
   }
 
   this.getSpecials = function() {
-    return $http.get(API_URL + '/featured')["specials"];
+    return $http.get(API_URL + '/featuredcategories')["specials"];
   }
 
   this.getComingSoon = function() {
-    return $http.get(API_URL + '/featured')["coming_soon"];
+    return $http.get(API_URL + '/featuredcategories')["coming_soon"];
   }
 
   this.getGame = function(id) {
