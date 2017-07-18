ac24cd1e21364053b518f4591e78d907d8982e09
MitulMistry <MitulMistryDev@gmail.com>
2017-07-18 01:06:45 -0500

    Refactor grid function to take columns parameter

--- js/app/controllers/GamesController.js
+++ js/app/controllers/GamesController.js
@@ -1,34 +1,24 @@
 function GamesController(games, $scope) { //games are injected from app.js resolve
   var ctrl = this;
   ctrl.games = games;
-  // $scope.grid = [[1,2,3],[4,5,6],[7,8,9]];
 
-
-  ctrl.createGrid = function() {
+  ctrl.createGrid = function(columns) { //grid needs to be something like: $scope.grid = [[1,2,3],[4,5,6],[7,8,9]];
     var returnGrid = [];
     var row = [];
 
     games.forEach(function(game, index) {
       row.push(game);
 
-      if( (index + 1) % 3 === 0) { //+1  to offset 0 index
+      if( (index + 1) % columns === 0) { //+1  to offset 0 index
         returnGrid.push(row);
         row = [];
       }
     });
 
     return returnGrid;
-
-    // for(var i = 0; i < i.length; i++) {
-    //
-    //   for(var j = 0; j < 3; j++) {
-    //
-    //   }
-    //
-    // }
   }
 
-  $scope.grid = ctrl.createGrid();
+  $scope.grid = ctrl.createGrid(3);
 }
 
 angular
