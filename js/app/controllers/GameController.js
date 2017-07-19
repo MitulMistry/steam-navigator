4a9cc334885491ffaf2f80a80788f1e6fa0faf07
MitulMistry <MitulMistryDev@gmail.com>
2017-07-19 01:24:37 -0500

    Set up html binding for game descriptions in show pages

--- js/app/controllers/GameController.js
+++ js/app/controllers/GameController.js
@@ -1,6 +1,9 @@
-function GameController(game) { //game is injected from app.js resolve
+function GameController(game, $scope, $sce) { //game is injected from app.js resolve
   var ctrl = this;
   ctrl.game = game;
+
+  $scope.shortDescription = $sce.trustAsHtml(game.data.short_description); //https://stackoverflow.com/a/31333196
+  $scope.detailedDescription = $sce.trustAsHtml(game.data.detailed_description);
 }
 
 angular
