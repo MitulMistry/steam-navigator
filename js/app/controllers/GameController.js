965fa93ce91f350a0d9adf6adb222da3349a53b8
MitulMistry <MitulMistryDev@gmail.com>
2017-07-20 12:14:05 -0500

    Display nested data for game show page

--- js/app/controllers/GameController.js
+++ js/app/controllers/GameController.js
@@ -5,7 +5,29 @@ function GameController(game, $scope, $sce) { //game is injected from app.js res
   $scope.shortDescription = $sce.trustAsHtml(game.data.short_description); //https://stackoverflow.com/a/31333196
   $scope.detailedDescription = $sce.trustAsHtml(game.data.detailed_description);
 
+  $scope.getDevelopers = function() {
+    return game.data.developers.join();
+  }
 
+  $scope.getPublishers = function() {
+    return game.data.publishers.join();
+  }
+
+  $scope.getCategories = function() {
+    var string = new String();
+    game.data.categories.forEach(function(category) {
+      string += (category.description + ', ');
+    });
+    return string;
+  }
+
+  $scope.getGenres = function() {
+    var string = new String();
+    game.data.genres.forEach(function(genre) {
+      string += (genre.description + ', ');
+    });
+    return string;
+  }
 }
 
 angular
