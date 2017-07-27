70372f517f6d4e7c38d8635d00132e73da613fb4
MitulMistry <MitulMistryDev@gmail.com>
2017-07-27 12:56:44 -0500

    Set up Express proxy API requests on dev server

--- devServer.js
+++ devServer.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var cors = require('cors');
+var request = require('request');
 var app = express();
 
 app.use(cors());
 app.use(express.static(__dirname));
+
+app.get('/', function(req, resp) {
+  resp.render('index.html');
+});
+
+// API ---
+var API_URL = 'https://store.steampowered.com/api';
+
+app.get('/api/featuredcategories', function(req, resp) {
+  req.pipe(request(API_URL + '/featuredcategories')).pipe(resp);
+});
+
+app.get('/api/appdetails/:appids', function(req, resp) {
+  req.pipe(request(API_URL + '/appdetails?appids=' + req.params.appids)).pipe(resp);
+});
 
 app.listen(8080);
 
