ddefae1b3d56a0dcd382337285e01461fa9b1a6c
MitulMistry <MitulMistryDev@gmail.com>
2017-08-03 17:38:59 -0500

    Delete unneeded comments

--- js/app/directives/ShowOnLoad.js
+++ js/app/directives/ShowOnLoad.js
@@ -1,14 +1,11 @@
 function ShowOnLoad() {
   return {
-    restrict: 'A',
+    restrict: 'A', //restrict use as attribute
     link: function(scope, element, attrs) {
       element.bind('load', function() {
-        // element.addClass('animationIf');
-        scope.$apply(function() {
-          // element.addClass('test-red'); //animationIf
+        scope.$apply(function() { // can add class element if needed, ex: element.addClass('animationIf');
           scope.visible = true; //sets $scope.visible variable to true, needs to be set to false initially in controller, element ng-show="visible", and class="fade" to trigger animation
         });
-        // console.log('Image is loaded');
       });
       element.bind('error', function(){
         console.log('Image could not be loaded');
