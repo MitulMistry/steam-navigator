9fe1d21be71be78e64613d0e936bad0a79dbb825
MitulMistry <MitulMistryDev@gmail.com>
2017-08-02 22:31:46 -0500

    Extract screenshots into component and implement in grid on game show page

---
+++ js/app/components/Screenshot.js
@@ -0,0 +1,15 @@
+var Screenshot = {
+  templateUrl: 'views/games/_screenshot.html',
+  bindings: { //define arguments being passed to component - bindings for components, scope for directives
+    data: '=' //set data to a variable being passed in, i.e. when used: <item data="post"></item>
+  },
+  controller: function($scope) { //can add functionality to a controller for this component by injecting dependencies such as a service
+    var ctrl = this;
+    $scope.visible = false;
+  },
+  controllerAs: 'ctrl' //sets controller to be refered to as game in the template
+};
+
+angular
+  .module('app')
+  .component('screenshot', Screenshot); //lowercase string name because no capitalization in DOM element when this component is used
