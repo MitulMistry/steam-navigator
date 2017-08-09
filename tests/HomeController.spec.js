69390a433d369316edb07150872aea287222b77a
MitulMistry <MitulMistryDev@gmail.com>
2017-08-08 20:23:34 -0500

    Add HomeController test

---
+++ tests/HomeController.spec.js
@@ -0,0 +1,17 @@
+describe('HomeController', function() {
+  beforeEach(module('app'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+  it('should have current year', function() {
+    var $scope = {};
+
+    var controller = $controller('HomeController', { $scope: $scope });
+    expect(controller.date).toEqual(jasmine.any(Date)); //should equal a Date object
+    expect(controller.date.getFullYear()).toEqual(new Date().getFullYear()); //should equal current year
+  });
+});
